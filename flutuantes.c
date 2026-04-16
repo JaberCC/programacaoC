604cdcd18a8054835af42dfc2aa457bfa03aeaac
Jáber Campos <136126497+JaberCC@users.noreply.github.com>
2026-04-16 23:53:50 +0000

    Operação com números flutuantes.

---
+++ flutuantes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+    float x = 6.6;
+    float y = 2.3;
+    float Soma = x + y;
+    float Diferenca = x - y;
+    float Produto = x * y;
+    float Quociente = x / y;// Divisão com ponto flutuante.
+
+    printf ("Soma: %.2f\n", Soma);
+    printf ("Diferença: %.2f\n", Diferenca);
+    printf ("Produto: %.2f\n", Produto);
+    printf ("Quociente: %.2f\n", Quociente);
+
+return 0;
+
+
+
+}
+
