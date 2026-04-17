7603113a9c32d1e3e8fd803351beb1c9eff1310a
Jáber Campos <136126497+JaberCC@users.noreply.github.com>
2026-04-17 00:04:12 +0000

    Conversão de inteiro para flutuante.

---
+++ conversao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+
+    int a = 10;
+    float b = 5.5;
+    float resultado = a / b; //Resultado convertido de inteiro para flutuante.
+
+    printf ("a dividido por b terá como quociente: %.3f\n", resultado);
+    
+
+}
